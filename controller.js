c41af77a173ee06e6ed48a570a28b9dabebff360
rynptr <rianputra2302@gmail.com>
2021-04-27 05:05:45 +0700

    zxc

--- controller.js
+++ controller.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var response = require('./res');
-//var connection = require('./conn');
+var connection = require('./conn');
+var jwt = require('jsonwebtoken');
 
 exports.index = function(req, res) {
     response.ok("Hello from the Node JS RESTful side!", res)
