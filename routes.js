f8c3dc6e216df97ae317f79810e6bd76cfa6bd4e
rynptr <rianputra2302@gmail.com>
2021-04-27 02:25:56 +0700

    asdadadad

--- routes.js
+++ routes.js
@@ -42,9 +42,7 @@ module.exports = function(app) {
         resolve.render("view_pembangunan")
     });
 
-
-    app.route('/listusulan')
-        .get(todoList.usulan);
+    app.route("/listusulan").get(todoList.usulan);
 
 
 };
